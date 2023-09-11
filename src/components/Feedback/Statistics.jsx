75faca7860ba83d9da9fc4e14f3e9fdba5da1e54
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2023-09-11 20:03:13 +0200

    і

---
+++ src/components/Feedback/Statistics.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Statistics = ({good, neutral, bad, total, positivePercentage}) => {
+    return (
+        <>
+        <h1>Statistics</h1>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        </>
+    )
+}
